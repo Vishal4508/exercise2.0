75febf38778225183f2830b96d7cc80e52ea0d06
Vishal4508 <40260749+Vishal4508@users.noreply.github.com>
2018-06-26 12:37:05 +0530

    Update oneToN.java
    added input

--- oneToN.java
+++ oneToN.java
@@ -1,22 +1,16 @@
 package practice2;
 
+import java.util.Scanner;
+
 public class oneToN {
-/*	static int p;
-	public int getUserInput(int q) {
-	p = q;
-	return p;
-	}
-	
-public static void main(String[] args) {
-	System.out.println("entered number is :"+p);
-	
-	
-}*/
-	public static void main(String[] args) {
-	int inputnum = 5;	
-	int i;
-	for (i = 0; i<inputnum ; i++) {
-		System.out.println(i);
-	}
-	}		
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.print("print numbers upto :");
+		int i = scan.nextInt();
+		int j = 1;
+		for (j =1; j <= i; j++ ){
+			System.out.println(j);
+		}
+		}
+
 }
