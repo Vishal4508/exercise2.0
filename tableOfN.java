b4ae2ad43742294e42acbbe608dba72ff5952271
Vishal4508 <40260749+Vishal4508@users.noreply.github.com>
2018-06-26 12:40:40 +0530

    Update tableOfN.java
    added input

--- tableOfN.java
+++ tableOfN.java
@@ -1,18 +1,19 @@
 package practice2;
-
+import java.util.Scanner;
 public class tableOfN {
-	public static void main(String[] args) {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int i ;
+		System.out.println("enter a number");
+		
+		int j = scan.nextInt();
 		
 		
-		int tableOf = 16;//change this value
-		
-		
-		int i = tableOf;
-		int j;
-		for(j =1; j<=10; j++) {
-			System.out.println(i+"X"+j+ "= " +i*j);
+		for (i = 1; i <=10; i++ ){
+			System.out.println(j+"x"+i+" ="+i*j );
 		}
-		
+		scan.close();
 	}
 
 }
+
